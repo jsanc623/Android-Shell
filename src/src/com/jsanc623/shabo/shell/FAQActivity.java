cc7c7f537c03e9adfcd15e12f8a2936780952d83
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2012-12-28 13:31:26 -0500

    Added text + global button styling

---
+++ src/src/com/jsanc623/shabo/shell/FAQActivity.java
@@ -0,0 +1,12 @@
+package com.jsanc623.shabo.shell;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FAQActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
