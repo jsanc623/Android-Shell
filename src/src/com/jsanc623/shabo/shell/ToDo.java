e71add174d7f5eddfe0fc86ad6700961b39e5dfa
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2013-01-02 03:30:17 -0500

    Added todo item

--- src/src/com/jsanc623/shabo/shell/ToDo.java
+++ src/src/com/jsanc623/shabo/shell/ToDo.java
@@ -4,6 +4,7 @@ package com.jsanc623.shabo.shell;
  * FIXED: Require password button redundant, removed
  * FIXED: Require password "button save" text change to "save password"
  * FIXED: Capture image bug: no return (common android bug)
+ * TODO: Play music on widget open
  * TODO: On widget click, show all data (need to fix Capture Image Bug first)
  * TODO: Force close on return from capture image intent
  */
